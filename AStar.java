e895fbd59d775aabc5ab633dca451c544a8a429b
Saba-Heydaridoost <saba.heydaridoost79@gmail.com>
2022-06-20 20:20:14 +0430

    AStar is created and Car's x & Y are not private anymore

---
+++ AStar.java
@@ -0,0 +1,60 @@
+package com.company;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class AStar {
+
+    public Scanner in;
+    public PriorityQueue<State> priorityQueue;
+
+    private LinkedList<State> aStarSearch(Board board, Heuristic heuristic) {
+
+    }
+
+}
+
+
+
+public abstract class Heuristic {
+
+    public Heuristic() {}
+
+    public abstract int getValue(State state);
+
+}
+
+public class Heuristic1 extends Heuristic{
+
+    @Override
+    public int getValue(State state) {
+        Car redCar = state.puzzle.getRedCar();
+        int gridSize = state.puzzle.gridSize;
+        int numberOfMovements = 0;
+        for (int i = redCar.y + 2; i < gridSize; i++) {
+            int x = redCar.x;
+            int y = i;
+            if(state.puzzle.crashCars(x, y)){
+                Car car = state.puzzle.crashedCar;
+                if(!state.puzzle.canMoveDown(car) && !state.puzzle.canMoveUp(car))
+                    numberOfMovements += 2;
+                else
+                    numberOfMovements ++;
+            }
+        }
+        return numberOfMovements;
+    }
+
+}
+
+public class Heuristic2 extends Heuristic{
+
+    @Override
+    public int getValue(State state) {
+        return 0;
+    }
+
+}
---
+++ Car.java
@@ -0,0 +1,65 @@
+package com.company;
+
+public class Car {
+    int x;
+    int y;
+    private String orient;
+    private int size;
+    /**
+ *
+ * @param orient   the orientations of each car
+ * @param size     the sizes of each car
+ * @param x        the x-coordinates of each car
+ * @param y        the y-coordinates of each car
+ */
+    Car(int x, int y, String orient, int size){
+        this.x = x;
+        this.y = y;
+        this.orient = orient;
+        this.size = size;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public String getOrient() {
+        return orient;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void moveDown() {
+        this.x ++;
+    }
+
+    public void moveUp() {
+        this.x --;
+    }
+
+    public void moveRight() {
+        this.y ++;
+    }
+
+    public void moveLeft() {
+        this.y --;
+    }
+
+    public boolean isHorizontal() {
+        return orient.equals("h");
+    }
+
+    public boolean isVertical() {
+        return orient.equals("v");
+    }
+
+    public Car clone(){
+        return new Car(this.x, this.y, this.orient, this.size);
+    }
+}
