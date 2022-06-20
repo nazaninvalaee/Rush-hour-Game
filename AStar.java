e4a3b0a506dd3e8f687b396bf9a48a54e9a5591b
Saba-Heydaridoost <saba.heydaridoost79@gmail.com>
2022-06-20 20:36:09 +0430

    Heuristic added

--- AStar.java
+++ AStar.java
@@ -19,34 +19,5 @@ public class AStar {
 
 
 
-public class Heuristic1 extends Heuristic{
 
-    @Override
-    public int getValue(State state) {
-        Car redCar = state.puzzle.getRedCar();
-        int gridSize = state.puzzle.gridSize;
-        int numberOfMovements = 0;
-        for (int i = redCar.y + 2; i < gridSize; i++) {
-            int x = redCar.x;
-            int y = i;
-            if(state.puzzle.crashCars(x, y)){
-                Car car = state.puzzle.crashedCar;
-                if(!state.puzzle.canMoveDown(car) && !state.puzzle.canMoveUp(car))
-                    numberOfMovements += 2;
-                else
-                    numberOfMovements ++;
-            }
-        }
-        return numberOfMovements;
-    }
 
-}
-
-public class Heuristic2 extends Heuristic{
-
-    @Override
-    public int getValue(State state) {
-        return 0;
-    }
-
-}
